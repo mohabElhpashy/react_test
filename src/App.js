2edcc97f9d2e5db4befcbafce766e33f53d93a2b
youssef ilsaqa <youssef_ilsaqa@hotmail.com>
2020-12-26 17:38:48 +0200

    b3basa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 // import Axios from "axios";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -10,17 +10,17 @@ import Nav from "./Components/LayOut/Layout";
 import Post from "./Components/Post/Post";
 import Allposts from "./Components/Allposts/Allposts";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <Router>
         <Nav />
         <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/signup" component={Signup} />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/post" component={Post} />
-          <Route exact path="/allposts" component={Allposts} />
+          <Route path="/" exact component={LoginPage} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/login"  exact component={Home} />
+          <Route path="/post" exact component={Post} />
+          <Route path="/allposts" exact component={Allposts} />
         </Switch>
       </Router>
     );
